a50f5fbbf0056c92d2b61a3978a84f0ca0aa1abb
Adib Aayan <mohin31@gmail.com>
2020-07-26 10:39:22 -0700

    Finished

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ class Player:
   def __init__(self, name):
     self.level = 1
     self.name = name
-    self.health = 110
+    self.health = 105
     self.title = "Rookie "
 
   def __str__(self):
@@ -50,7 +50,10 @@ def game():
     if 'player' not in session:
       session['player'] = Player(submitted_text).toJSON()
       print(session['player'])
-
+    if 'last_move' not in session:
+        session['last_move'] = None
+    if 'last_message' not in session:
+        session['last_message'] = None
     # Only go to play page if the player has been created
     return redirect(url_for('play')) # MUST use `return` keyword for redirect
   return render_template('game.html', form=form)
@@ -58,39 +61,77 @@ def game():
 
 @app.route('/play', methods = ["GET","POST"])
 def play():
+  # Deletes previous messages:
+  session.pop('_flashes', None)
   player = session['player']
-  print(player)
-  choices = ["sleeping", "training", "eating breakfast", "eating lunch", "eating dinner", "in trouble", "under attack"]
-  print(choices)
-  alive = True
+  if player['level'] >= 25:
+      player['title'] = "General "
+  elif player['level'] >= 10:
+      player['title'] = "Master "
+  player['health'] = player['health'] - 5
+  if player['health'] <= 0:
+      return redirect(url_for('game_over'))
   form = ChoicesForm() # This is a setup code
-  print(player)
+  flash("Name: " + player['title'] + " " + player['name'])
+  flash("Health: " + str(player['health']))
+  flash("Level: " + str(player['level']))
+  if session['last_message'] is not None:
+      flash(session['last_message'])
   # Functionality code
+  random_float = random.random()
   if form.validate_on_submit():
     # Form submitted
     submitted_text = form.text.data
-    if submitted_text == "eating lunch" or submitted_text == "eating dinner" or submitted_text == "eating breakfast":
-      print("Eat healthy!")
-      player['health'] = player['health'] + 25
-    elif submitted_text == "sleeping":
-      print("Good night!")
-      player["level"] = player["level"] + 1
-    elif submitted_text == "training":
-        print("Good luck!")
-        player["level"] = player["level"] + 1
-    elif submitted_text == "in trouble" or submitted_text == "under attack":
-      print("WATCH OUT!")
-      under_attack = True
+    # submitted_text is the user's current move that they typed in.
+    if session['last_move'] == "under attack":
+        session['last_message'] = "WATCH OUT! What is your plan? (Choices: fight back, defend, run away)"
+        if submitted_text == 'fight back':
+            if random_float <= 0.49:
+                player['health'] = player['health'] - 35
+                session['last_message'] = 'Ouch! You lost 35 health.'
+            else:
+                player["level"] = player["level"] + 10
+        elif submitted_text == 'defend':
+            if random_float <= 0.25:
+                session['last_message'] = 'Ouch! You lost 25 health!'
+                player['health'] = player['health'] - 25
+            else:
+                session['last_message'] = "Hooray! You succeeded!"
+                player["level"] =  player["level"] + 2
+        elif submitted_text == 'run away':
+            session['last_message'] = "You ran away, lost 0 health."
     else:
-        print("You don't want to do that...")
-    print(submitted_text)
-    flash(u'Submitted successfully', 'success')
+        if submitted_text == "eating lunch" or submitted_text == "eating dinner" or submitted_text == "eating breakfast":
+          session['last_message'] = "Eat healthy!"
+          player['health'] = player['health'] + 25
+        elif submitted_text == "sleeping":
+          session['last_message'] = "Good night!"
+          player["level"] = player["level"] + 1
+          player['health'] = player['health'] + 10
+        elif submitted_text == "training":
+            session['last_message'] = "Good luck!"
+            player["level"] = player["level"] + 2.5
+        elif submitted_text == "in trouble" or submitted_text == "under attack":
+          session['last_message'] = "WATCH OUT! What's your plan? (Choices: fight back, defend, run away.)"
+          under_attack = True
+        else:
+            flash("You don't want to do that...")
+
+    # These actions should happen after every turn.
+    print("Successful")
+    session['last_move'] = submitted_text
     return redirect(url_for('play'))  # reload game page if not done playing
+
+
   else:
     # Form submission failed
-    flash(u'Submission failed', 'danger')
+    print("Failed")
   return render_template('play.html', form=form) # Render the screen
 
+@app.route('/game_over')
+def game_over():
+  session.clear()
+  return render_template('game_over.html')
 
 @app.route('/about')
 def about():
